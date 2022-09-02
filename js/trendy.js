b0f31f8b6ba163892c2cdd1629064495d45b5017
Rabbe <onikmahmudrabbe@gmail.com>
2022-09-03 01:34:50 +0600

    Load news,Fetch & console the data done

--- js/trendy.js
+++ js/trendy.js
@@ -6,16 +6,25 @@ const loadCategory = async () => {
 }
 const displayCategory = async (categories) => {
     const categoryContainer = document.getElementById('category-container');
-    categoryContainer.innerText = `Home`;
     categories.forEach(category => {
         console.log(category);
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('d-inline-block', 'px-4', 'my-4')
         categoryDiv.innerHTML = `
-        <p> ${category.category_name}</p>
+        <p onclick="loadNews('https://openapi.programming-hero.com/api/news/category/${category.category_id}')" class="btn btn-white"> ${category.category_name}</p>
         `;
         categoryContainer.appendChild(categoryDiv);
     })
+
+}
+
+const loadNews = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
+
+const displayNews = async (news) => {
 
 }
 
