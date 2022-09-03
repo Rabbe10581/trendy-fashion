f82296db01c209d2bfb0e2b19cf104d026dec10d
Rabbe <onikmahmudrabbe@gmail.com>
2022-09-03 18:24:46 +0600

    author image added

--- js/trendy.js
+++ js/trendy.js
@@ -46,10 +46,13 @@ const displayNews = async (newsAll) => {
             <div class="card flex-column flex-md-row align-items-center">
                 <img class="card-img-top img-fluid w-25" src="${news.image_url}" alt="...">
                 <div class="card-body flex-row">
-                        <h5 class="card-title fs-4 fw-semibold">${news.title}</h5>
-                        <p class="card-text">${news.details.slice(0, 250)}</p>
+                        <h5 class="card-title fs-4 pb-2 fw-semibold">${news.title}</h5>
+                        <p class="card-text fw-light">${news.details.slice(0, 250)}</p>
                         <div class="d-flex flex-row justify-content-between px-3">
-                            <p class="fw-semibold">${news.author.name}</p>
+                            <div class="d-flex flex-row">
+                                <img class="author-img rounded-circle" src="${news.author.img}" alt="">
+                                <p class="fw-semibold ps-2">${news.author.name}</p>
+                            </div>
                             <p class="fw-semibold">Views: ${news.total_view}</p>
                             <button onclick="loadPostDetails('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailsPostModal">Details</button>
                         </div>
@@ -89,10 +92,11 @@ const loadPostDetails = async id => {
 }
 
 const displayPostDetails = post => {
-    console.log(post);
+    // console.log(post);
     const modalTitle = document.getElementById('detailsPostModalLabel');
     modalTitle.innerText = post[0].author.name ? post[0].author.name : `No Author Available`;
     const modalBody = document.getElementById('viewsTotal');
     modalBody.innerText = `Views: ${post[0].total_view ? post[0].total_view : `Not Found`}`;
 }
+loadNews('https://openapi.programming-hero.com/api/news/category/08')
 loadCategory()
